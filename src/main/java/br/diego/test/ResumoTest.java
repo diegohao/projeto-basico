782e71ecabf51475e5250e5857b40b5295f6cec8
Diego <diegobr@hotmail.com>
2023-02-14 19:13:04 -0300

    Refactoring test2ResumoMensal

--- src/main/java/br/diego/test/ResumoTest.java
+++ src/main/java/br/diego/test/ResumoTest.java
@@ -1,8 +1,12 @@
 package br.diego.test;
 
+import java.util.List;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import br.diego.core.BaseTeste;
 import br.diego.core.DriverFactory;
@@ -28,7 +32,8 @@ public class ResumoTest extends BaseTeste {
 		menuPage.acessarTelaResumo();
 		
 		Assert.assertEquals("Seu Barriga - Extrato", DriverFactory.getDriver().getTitle());
+		
+		List<WebElement> elementosEncontrados = DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+		Assert.assertEquals(0, elementosEncontrados.size());
 	}
-	
-
 }
